e30fde8e1f4365e79cb5e6c98f294428c6b61aa4
Sean Eriksen <eriksensean@yahoo.com>
2019-10-22 19:01:52 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,26 +46,29 @@ def naming_system
    label: "Kingdom",
    sub_category: {
      label: "Phylum",
-     sub_category: {        label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
-  
+     sub_category: {
+       label: "Class"
+     }
+   }
+ },
+ 
+ PORTION_2 = {
+   label: "Order"
+ },
+ 
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ },
+ 
+ PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+ }
+ 
+ }
+   
 
 end
