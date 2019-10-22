bc45f05f4e8437bf0f616a8e46d06a76784438af
Sean Eriksen <eriksensean@yahoo.com>
2019-10-22 18:59:41 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,11 +43,10 @@ def naming_system
   # HoH all the way to Species!
   
   classification = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {        label: "Class"
 #     }
 #   }
 # }
